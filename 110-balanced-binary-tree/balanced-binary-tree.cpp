8f09f3d0a6d2d2f2d30dc975176cc6f381535766
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-02-03 23:15:17 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (54.64%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+
+        int lh = height(root->left);
+        if (lh == -1) return -1;
+
+        int rh = height(root->right);
+        if (rh == -1) return -1;
+
+        if (abs(lh - rh) > 1)
+            return -1;
+
+        return 1 + max(lh, rh);
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return height(root) != -1;
+    }
+};
