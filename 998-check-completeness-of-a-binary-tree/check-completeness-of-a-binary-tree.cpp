452de6e72e346ec7e03146305fe8b8e80aee1e36
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-02-04 22:08:56 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (98.20%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
@@ -1,25 +1,48 @@
 
+// class Solution {
+// public:
+//     bool isCompleteTree(TreeNode* root) {
+//         bool past=false;
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         while(!q.empty()){
+//             TreeNode *node=q.front();
+//             q.pop();
+//             if(node==NULL){
+//                 past=true;
+//             }
+//             else{
+//                 if(past==true){
+//                     return false;
+//                 }
+//                 q.push(node->left);
+//                 q.push(node->right);
+//             }
+
+//         }
+//         return true;
+//     }
+// };
+
+//dfs
 class Solution {
 public:
-    bool isCompleteTree(TreeNode* root) {
-        bool past=false;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode *node=q.front();
-            q.pop();
-            if(node==NULL){
-                past=true;
-            }
-            else{
-                if(past==true){
-                    return false;
-                }
-                q.push(node->left);
-                q.push(node->right);
-            }
-
+    int countNodes(TreeNode * root){
+        if(root==NULL){
+            return 0;
         }
-        return true;
+        return 1+countNodes(root->left)+countNodes(root->right);
+    }
+    bool dfs(TreeNode *root,int i,int totalNodes){
+        if(root==NULL) return true;
+
+        if(i>totalNodes) return false;
+
+        return dfs(root->left,2*i,totalNodes) && dfs(root->right,2*i+1,totalNodes);
+    }
+    bool isCompleteTree(TreeNode* root) {
+            int totalNodes=countNodes(root);
+            int i=1;
+            return dfs(root,i,totalNodes);
     }
 };
